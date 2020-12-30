5eb9b9c1797ea4bc7f16081802f766a0740c2df0
SadColor <60848907+SadColor@users.noreply.github.com>
2020-12-30 10:34:56 -0600

    yeeeaarrgh

--- bug/kyara/page.js
+++ bug/kyara/page.js
@@ -1,14 +1,17 @@
-var btns = document.querySelectorAll(".collapse");
-var divs = document.querySelectorAll(".movelist");
+window.addEventListener("load", loadCollapse);
 
-function collapse() {
-	this.parentElement.nextElementSibling.classList.toggle("hide");
-}
+function loadCollapse() {
+	var btns = document.querySelectorAll(".collapse");
+	var divs = document.querySelectorAll(".movelist");
 
-for (var i = 0; i < divs.length; i++) {
-	divs[i].style.maxHeight = divs[i].scrollHeight + "px";
-}
-for (var i = 0; i < btns.length; i++) {
-	btns[i].addEventListener("click", collapse);
-}
+	function collapse() {
+		this.parentElement.nextElementSibling.classList.toggle("hide");
+	}
 
+	for (var i = 0; i < divs.length; i++) {
+		divs[i].style.maxHeight = divs[i].clientHeight + "px";
+	}
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", collapse);
+	}
+}
