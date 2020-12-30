69962b6c87eb0c698a46f6472d2e698a0e705898
SadColor <60848907+SadColor@users.noreply.github.com>
2020-12-30 00:34:32 -0600

    bedtime

--- bug/kyara/page.js
+++ bug/kyara/page.js
@@ -1,13 +1,9 @@
 var btns = document.querySelectorAll(".collapse");
-var divs = document.querySelectorAll(".movelist");
 
 function collapse() {
 	this.parentElement.nextElementSibling.classList.toggle("hide");
 }
 
-for (var i = 0; i < divs.length; i++) {
-	divs[i].style.maxHeight = divs[i].clientHeight;
-}
 for (var i = 0; i < btns.length; i++) {
 	btns[i].addEventListener("click", collapse);
 }
