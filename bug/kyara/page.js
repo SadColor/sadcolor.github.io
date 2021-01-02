58cb276ce7aa654fb5092d26a649f323199c882c
SadColor <60848907+SadColor@users.noreply.github.com>
2021-01-02 05:28:03 -0600

    nearly finalized layout, attributes page

--- bug/kyara/page.js
+++ bug/kyara/page.js
@@ -1,16 +1,19 @@
 window.addEventListener("load", loadCollapse);
+window.addEventListener("resize", measure);
 
-function loadCollapse() {
-	var btns = document.querySelectorAll(".collapse");
+function measure() {
 	var divs = document.querySelectorAll(".movelist");
+	for (var i = 0; i < divs.length; i++) {
+		divs[i].style.maxHeight = divs[i].scrollHeight + "px";
+	}
+}
+function loadCollapse() {
+	measure();
+	var btns = document.querySelectorAll(".collapse");
 
 	function collapse() {
 		this.parentElement.nextElementSibling.classList.toggle("hide");
-	}
-
-	for (var i = 0; i < divs.length; i++) {
-		divs[i].style.maxHeight = divs[i].clientHeight + "px";
-	}
+	}	
 	for (var i = 0; i < btns.length; i++) {
 		btns[i].addEventListener("click", collapse);
 	}
