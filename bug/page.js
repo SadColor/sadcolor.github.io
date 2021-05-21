abd9e9bfb132887e4e4505e9b461c622406b2271
SadColor <60848907+SadColor@users.noreply.github.com>
2021-05-21 07:38:36 -0500

    brush up formatting on log #0, fix js, add scroll margin to headers

--- bug/page.js
+++ bug/page.js
@@ -1,13 +1,10 @@
 window.addEventListener("load", loadCollapse);
 window.addEventListener("resize", measure);
 
-function measure() {
-	var divs = document.querySelectorAll(".movelist");
-	for (var i = 0; i < divs.length; i++) {
-		divs[i].style.maxHeight = divs[i].scrollHeight + "px";
-	}
-}
 function loadCollapse() {
+	divs = document.querySelectorAll(".movelist");
+	headSp = document.querySelector("#headspace");
+	headC = document.querySelector("#headcont");
 	measure();
 	var btns = document.querySelectorAll(".collapse");
 
@@ -17,4 +14,10 @@ function loadCollapse() {
 	for (var i = 0; i < btns.length; i++) {
 		btns[i].addEventListener("click", collapse);
 	}
+}
+function measure() {
+	for (var i = 0; i < divs.length; i++) {
+		divs[i].style.maxHeight = divs[i].scrollHeight + "px";
+	}
+	headSp.style.paddingBottom = headC.scrollHeight + "px";
 }
